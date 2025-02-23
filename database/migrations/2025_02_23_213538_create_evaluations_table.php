3a6bbf830916e468088b9885144d9e2aabba81f4
Charmaine Cator <charmaine.l.d.cator@gmail.com>
2025-02-24 05:45:05 +0800

    New tables has been added

---
+++ database/migrations/2025_02_23_213538_create_evaluations_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvaluationsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('schedule_id')->constrained()->onDelete('cascade');
+            $table->string('evaluator_name');
+            $table->dateTime('evaluation_date');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+}
---
+++ database/migrations/2025_02_23_213547_create_evaluation_details_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvaluationDetailsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('evaluation_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('evaluation_id')->constrained('evaluations')->onDelete('cascade');
+            $table->foreignId('criteria_id')->constrained('criterias')->onDelete('cascade');
+            $table->tinyInteger('rating');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('evaluation_details');
+    }
+}
